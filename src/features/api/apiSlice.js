5cce7cd46475b08fccc0c77a4115ce22651720b1
rana-arju <arjurana20@gmail.com>
2023-01-23 05:46:44 -0800

    update backend live link

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://job-board-server-h5cb.onrender.com",
+    baseUrl: "https://job-board-server.vercel.app",
   }),
   tagTypes: ["jobs", "job"],
   endpoints: (builder) => ({}),
